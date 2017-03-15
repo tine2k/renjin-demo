16826fbf5c2c288537f257f9c572c77535145689
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2017-03-15 23:25:51 +0100

    weitere tests

--- src/main/java/org/tine2k/net/tine2k/RenjinTest.java
+++ src/main/java/org/tine2k/net/tine2k/RenjinTest.java
@@ -1,9 +1,11 @@
 package org.tine2k.net.tine2k;
 
+import org.renjin.eval.Session;
+import org.renjin.eval.SessionBuilder;
+import org.renjin.primitives.vector.RowNamesVector;
 import org.renjin.script.RenjinScriptEngineFactory;
-import org.renjin.sexp.ListVector;
-import org.renjin.sexp.NamedValue;
-import org.renjin.sexp.Vector;
+import org.renjin.sexp.*;
+import org.renjin.util.DataFrameBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
+import java.util.Arrays;
+import java.util.Map;
 
 @Component
 public class RenjinTest {
@@ -22,11 +26,16 @@ public class RenjinTest {
 
         logger.info("Lade R...");
 
+        Session session = new SessionBuilder()
+                .withDefaultPackages()
+                .build();
+
         RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
         ScriptEngine engine = factory.getScriptEngine();
 
         try {
-            ListVector eval = (ListVector) engine.eval("read.csv2(text=\"LD;Wert\nAT;8,3\nDE;80\", stringsAsFactors = FALSE);");
+            ListVector eval = (ListVector) engine.eval("a <- read.csv2(text=\"LD;Wert\nAT;8,3\nDE;80\nUS;120\", stringsAsFactors = FALSE);");
+            logger.info("" + eval);
             for (NamedValue x : eval.namedValues()) {
                 logger.info(x.getName() + "=" + x.getValue().getClass());
                 for (Object o : ((Iterable<?>) x.getValue())) {
@@ -35,7 +44,16 @@ public class RenjinTest {
             }
 
             engine.put("dp", this);
-            logger.info("" + engine.eval("dp$get(\"OS12345\")"));
+            logger.info("" + engine.eval("b <- dp$get(\"OS12345\")"));
+
+            logger.info("" + engine.eval("c <- merge(a,b,all=TRUE)"));
+
+            logger.info("" + engine.eval("names(b)"));
+
+            logger.info("" + engine.eval("c[order(-c$Wert),]$LD[1]"));
+
+            logger.info("" + engine.eval("library(compare); comparison <- compare(a,b,allowAll=TRUE); comparison$tM"));
+
 
         } catch (ScriptException e) {
             e.printStackTrace();
@@ -43,8 +61,15 @@ public class RenjinTest {
 
     }
 
-    public String get(String konzept) {
-        return "myData: " + konzept;
+    public ListVector get(String konzept) {
+
+        ListVector.NamedBuilder builder = new ListVector.NamedBuilder();
+        builder.add("LD", new StringArrayVector("FR","GR"));
+        builder.add("Wert", new DoubleArrayVector(50,20));
+        builder.setAttribute(Symbols.ROW_NAMES, new RowNamesVector(2));
+        builder.setAttribute(Symbols.CLASS, StringArrayVector.valueOf("data.frame"));
+
+        return builder.build();
     }
 
 }
